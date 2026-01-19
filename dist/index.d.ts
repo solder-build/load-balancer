e1c54af391be9b42e6767e17224eab4d6acba09c
Bao Ho Gia <54936754+BaoHG1508@users.noreply.github.com>
2026-01-19 10:37:51 +0700

    Add .gitignore and remove dist/ and node_modules/ from tracking

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { LoadBalancer } from "./loadBalancer.js";
-export type { EndpointConfig, EndpointStatus, LoadBalancerOptions, SelectedEndpoint, } from "./types.js";
-//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++
@@ -1,2 +0,0 @@
-export { LoadBalancer } from "./loadBalancer.js";
-//# sourceMappingURL=index.js.map
